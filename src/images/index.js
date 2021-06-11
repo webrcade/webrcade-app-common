cfb0278cc2a14cd48f6fa7950332f222535c173e
raz0red <raz0red@gmail.com>
2021-06-11 09:43:47 -0700

    Starting to add feeds

--- src/images/index.js
+++ src/images/index.js
@@ -1,4 +1,10 @@
+import AddCircleBlackImage from "add_circle_outline_black_24dp.svg"
+import AddCircleWhiteImage from "add_circle_outline_white_24dp.svg"
 import ArrowBackWhiteImage from "arrow_back_white_24dp.svg"
+import CloudDownloadBlackImage from "cloud_download_black_24dp.svg"
+import CloudDownloadWhiteImage from "cloud_download_white_24dp.svg"
+import DeleteForeverBlackImage from "delete_forever_black_24dp.svg"
+import DeleteForeverWhiteImage from "delete_forever_white_24dp.svg"
 import PlayArrowBlackImage from "play_arrow_black_24dp.svg"
 import PlayArrowWhiteImage from "play_arrow_white_24dp.svg"
 import VolumeOffBlack from "volume_off_black_24dp.svg"
@@ -6,7 +12,13 @@ import WebrcadeLogoDarkImage from "webrcade-logo-dark.svg"
 import WebrcadeLogoLargeImage from "webrcade-logo-large.svg"
 
 export {
+  AddCircleBlackImage,
+  AddCircleWhiteImage,
   ArrowBackWhiteImage,
+  CloudDownloadBlackImage,
+  CloudDownloadWhiteImage,
+  DeleteForeverBlackImage,
+  DeleteForeverWhiteImage,
   PlayArrowBlackImage,
   PlayArrowWhiteImage,
   WebrcadeLogoDarkImage,
--- src/resources/index.js
+++ src/resources/index.js
@@ -1,9 +1,14 @@
 const TEXT_IDS = {
+  ADD_UC: "ADD",
   CATEGORIES: "Categories",
+  DELETE_UC: "DELETE",
   ERROR_LOADING_GAME: "An error occurred attempting to load the selected game.",
   ERROR_RETRIEVING_GAME: "An error has occurred attempting to retrieve the selected game.",
   ERROR_UNKNOWN: "An unknown error has occurred.",
+  FEEDS: "Feeds",
+  LOAD_UC: "LOAD",
   LOADING: "Loading",
+  LOADING_FEED: "Loading feed...",
   OK: "OK",
   PLAY_UC: "PLAY",
   RESUME: "Resume",
@@ -11,6 +16,7 @@ const TEXT_IDS = {
   SEE_CONSOLE_LOG: "See console log for details.",
   SELECT_UC: "SELECT",
   SHOW_CATEGORIES: "Show Categories",
+  SHOW_FEEDS: "Show Feeds",
   SOMETHING_WENT_WRONG: "Whoops, something went wrong...",
   TAP_TO_UNMUTE: "Tap Screen to Unmute"
 };
