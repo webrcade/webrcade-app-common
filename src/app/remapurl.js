4b26e34ebfca8271652daf2fd4c89669b976cf7c
Chris Smith <raz0red@gmail.com>
2024-05-03 23:20:22 -0700

    Yet another Dropbox direct download fix

--- src/app/remapurl.js
+++ src/app/remapurl.js
@@ -16,6 +16,7 @@ const remapDropbox = (urlLower, url) => {
   if (urlLower.substring(0, DB_PREFIX.length) === DB_PREFIX) {
     if (urlLower.includes("&rlkey=") || urlLower.includes("?rlkey=")) {
       url = url.replace("dl=0", "dl=1");
+      url = url.replace(DB_PREFIX, "https://dl.dropbox.com/");
       LOG.info("Remapped dropbox url: '" + url + "'");
       return url;
     } else {
